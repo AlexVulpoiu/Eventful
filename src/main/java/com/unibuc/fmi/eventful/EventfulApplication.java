9d368791f1c603cd1b8917ffb04e4c702e657b41
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2024-02-04 22:10:34 +0200

    initial commit

---
+++ src/main/java/com/unibuc/fmi/eventful/EventfulApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.fmi.eventful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventfulApplication.class, args);
+	}
+
+}
